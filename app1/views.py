beabc310817b10d2899e5aa6f25229384882ee68
liya <2793955734@qq.com>
2020-09-03 00:04:34 +0800

    在app1下的view中新增了用户登录视图

--- app1/views.py
+++ app1/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index(request):
     print('1111')
     return HttpResponse('test')
+
+def user_logiin(request):
+    print('用户登录成功')
+    return HttpResponse('跳转到项目首页')
